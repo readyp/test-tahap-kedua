708814b472c32780bd8419007123e3c2837e08a3
readyp <readypamungkas@gmail.com>
2024-07-25 08:49:35 +0700

    Add .env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_CLIENT_SECRET: string
+  readonly VITE_CLIENT_ID: string
+  readonly VITE_GRANT_TYPE: string
+  readonly VITE_BASE_URL: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
